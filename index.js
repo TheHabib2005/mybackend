26a5f5fc2a6bf1ad5dde19ec224afcdd6f8b32cc
habib <mdwear2005@gmail.com>
2024-07-10 13:17:28 +0600

    h

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express"
+
+const app = express()
+
+
+let PORT = process.env.PORT || 8000;
+
+
+app.listen(PORT, () =>{
+    console.log(`Server running on port ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+    res.json({message:"succes "})
+})
