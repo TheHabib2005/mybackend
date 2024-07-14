3a05526cd22c294e8a2a430da687d9db2d9245b6
habib <mdwear2005@gmail.com>
2024-07-14 07:27:19 +0600

    h

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`)
 })
 app.use(cors({
-    origin: 'http://localhost:3000', // Replace with your frontend origin
+    origin: 'https://simple-inside-complex.vercel.app/', // Replace with your frontend origin
     credentials: true // 
 }))
 app.use(cookieParser());
